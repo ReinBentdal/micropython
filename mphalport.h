1c615642ade952e51f7b762ee917eb10ee729678
ReinBentdal <rein.bent@gmail.com>
2023-08-06 20:02:30 +0200

    working as lib

---
+++ mphalport.h
@@ -0,0 +1,6 @@
+
+#pragma once
+
+#include "py/mpconfig.h"
+
+void mp_hal_stdout_tx_strn(const char *str, mp_uint_t len);
---
+++ mpy.cmake
@@ -0,0 +1,55 @@
+
+set(MICROPY_DIR         ${CMAKE_SOURCE_DIR}/lib/micropython)
+set(MICROPY_PORT_DIR    ${MICROPY_DIR})
+set(MICROPY_TARGET      micropython)
+
+include(${MICROPY_DIR}/py/py.cmake)
+include(${MICROPY_DIR}/extmod/extmod.cmake)
+
+list(TRANSFORM MP_QSTR_EXT_SOURCES PREPEND ${CMAKE_SOURCE_DIR}/)
+
+set(MICROPY_SOURCE_QSTR
+    ${MICROPY_SOURCE_PY}
+    ${MICROPY_SOURCE_EXTMOD}
+    ${MICROPY_PORT_DIR}/mphalport.c
+    ${MICROPY_DIR}/shared/libc/printf.c
+    ${MICROPY_DIR}/shared/runtime/stdout_helpers.c
+    ${MP_QSTR_EXT_SOURCES}
+)
+
+zephyr_get_include_directories_for_lang(C includes)
+zephyr_get_system_include_directories_for_lang(C system_includes)
+zephyr_get_compile_definitions_for_lang(C definitions)
+zephyr_get_compile_options_for_lang(C options)
+
+set(MICROPY_CPP_FLAGS_EXTRA ${includes} ${system_includes} ${definitions} ${options})
+
+zephyr_library_named(${MICROPY_TARGET})
+
+zephyr_library_include_directories(
+    ${MICROPY_INC_CORE}
+    ${MICROPY_PORT_DIR}
+    ${CMAKE_CURRENT_BINARY_DIR}
+)
+
+zephyr_library_compile_options(
+    -std=gnu99 -fomit-frame-pointer
+)
+
+zephyr_library_compile_definitions(
+    NDEBUG
+    MP_CONFIGFILE=<${CONFIG_MICROPY_CONFIGFILE}>
+    MICROPY_HEAP_SIZE=${CONFIG_MICROPY_HEAP_SIZE}
+)
+
+zephyr_library_sources(${MICROPY_SOURCE_QSTR})
+
+add_dependencies(${MICROPY_TARGET} zephyr_generated_headers)
+
+include(${MICROPY_DIR}/py/mkrules.cmake)
+
+include_directories(
+    ${MICROPY_INC_CORE}
+    ${MICROPY_PORT_DIR}
+    ${CMAKE_CURRENT_BINARY_DIR}
+)
