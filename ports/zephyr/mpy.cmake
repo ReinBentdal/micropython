67687e6ca9c2c4359d3caa8d97a56aa67f3a36b5
ReinBentdal <rein.bent@gmail.com>
2023-08-06 19:22:29 +0200

    mpy.cmake simplify MICROPY_SOURCE_QSTR

--- ports/zephyr/mpy.cmake
+++ ports/zephyr/mpy.cmake
@@ -6,27 +6,13 @@ set(MICROPY_TARGET      micropython)
 include(${MICROPY_DIR}/py/py.cmake)
 include(${MICROPY_DIR}/extmod/extmod.cmake)
 
-set(MICROPY_SOURCE_PORT
-    src/main.c
-    mphalport.c
-)
-list(TRANSFORM MICROPY_SOURCE_PORT PREPEND ${MICROPY_PORT_DIR}/)
-
-set(MICROPY_SOURCE_SHARED
-    libc/printf.c
-    runtime/stdout_helpers.c
-)
-list(TRANSFORM MICROPY_SOURCE_SHARED PREPEND ${MICROPY_DIR}/shared/)
-
-set(MICROPY_SOURCE_LIB)
-list(TRANSFORM MICROPY_SOURCE_LIB PREPEND ${MICROPY_DIR}/lib/)
-
 set(MICROPY_SOURCE_QSTR
     ${MICROPY_SOURCE_PY}
     ${MICROPY_SOURCE_EXTMOD}
-    ${MICROPY_SOURCE_SHARED}
-    ${MICROPY_SOURCE_LIB}
-    ${MICROPY_SOURCE_PORT}
+    ${MICROPY_PORT_DIR}/mphalport.c
+    ${MICROPY_PORT_DIR}/src/main.c
+    ${MICROPY_DIR}/shared/libc/printf.c
+    ${MICROPY_DIR}/shared/runtime/stdout_helpers.c
 )
 
 zephyr_get_include_directories_for_lang(C includes)
